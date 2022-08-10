d1ebaf7ca0d524a588c06af2c966b2e7ab14e7be
Kartik Verma <56619907+Kar2410@users.noreply.github.com>
2022-08-11 00:54:41 +0530

    l

---
+++ src/LoginFireBase/auth/FirebaseAuth.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgr2zBeomha8EdryYkDDEjgyYw2024rMs",
+  authDomain: "reactloginauth-a1143.firebaseapp.com",
+  projectId: "reactloginauth-a1143",
+  storageBucket: "reactloginauth-a1143.appspot.com",
+  messagingSenderId: "270208945748",
+  appId: "1:270208945748:web:51b6142ffe4fbf138b67c1",
+  measurementId: "G-D41Q2L65B2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
